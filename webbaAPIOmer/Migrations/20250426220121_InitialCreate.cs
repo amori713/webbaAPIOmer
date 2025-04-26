6ab0053e34ec4240b26d29bed7e34708d81dc36d
Omer Chewai <Amori.1644@hotmail.com>
2025-04-27 00:10:15 +0200

    ny

--- webbaAPIOmer/Migrations/20250426220121_InitialCreate.cs
+++ webbaAPIOmer/Migrations/20250426220121_InitialCreate.cs
@@ -7,7 +7,7 @@ namespace webbaAPIOmer.Migrations
 {
     /// <inheritdoc />
     public partial class InitialCreate : Migration
-    {
+    { 
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
