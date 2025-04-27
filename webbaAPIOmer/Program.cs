4f17137a7485ce67de93f0c546bd1ebfee151e88
Omer Chewai <Amori.1644@hotmail.com>
2025-04-27 04:18:47 +0200

    new last

--- webbaAPIOmer/Program.cs
+++ webbaAPIOmer/Program.cs
@@ -11,35 +11,35 @@ namespace webbaAPIOmer
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Lägg till DbContext (EF Core + Connection String)
+            
             builder.Services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-            // Registrera din service (DI = Dependency Injection)
+            
             builder.Services.AddScoped<AdsService>();
 
-            // Lägg till Controllers
+            
             builder.Services.AddControllers();
 
-            // Lägg till Swagger
+            
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Tillämpa migrationer automatiskt när applikationen startar
+            
             using (var scope = app.Services.CreateScope())
             {
                 var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-                dbContext.Database.Migrate();  // Kör alla migrationer om de inte har körts ännu
+                dbContext.Database.Migrate();  
             }
 
-            // Aktivera Swagger direkt när appen startar
+            
             app.UseSwagger();
             app.UseSwaggerUI(options =>
             {
                 options.SwaggerEndpoint("/swagger/v1/swagger.json", "Ads API v1");
-                options.RoutePrefix = string.Empty; // Swagger startar på root
+                options.RoutePrefix = string.Empty; 
             });
 
             app.UseHttpsRedirection();
